2bf18495b7d364c471ffb3df6a8b012065c208af
recallmikey-web <recallmikey@gmail.com>
2025-11-24 06:54:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 关键配置：使用相对路径，避免在 GitHub Pages 子目录下出现 404 错误
 })
